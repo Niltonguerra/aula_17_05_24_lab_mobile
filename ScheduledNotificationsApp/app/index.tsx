316799f1e24a4deb5f5ceafc5561065c69f989cc
Nilton Dionisio Guerra <78931445+Niltonguerra@users.noreply.github.com>
2024-05-18 01:12:04 +0000

    subundo

--- ScheduledNotificationsApp/app/index.tsx
+++ ScheduledNotificationsApp/app/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Button, Platform } from 'react-native';
 import * as Notifications from 'expo-notifications';
+import * as Linking from 'expo-linking';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -9,6 +10,15 @@ Notifications.setNotificationHandler({
     shouldSetBadge: true,
   }),
 });
+Notifications.addNotificationResponseReceivedListener((response) => {
+  
+  const uri = response.notification.request.content.data.uri;
+  console.log('Received notification with uri:', uri);
+  if (uri) {
+    Linking.openURL(uri).catch((err) => console.error('An error occurred', err));
+  }
+});
+
 
 export default function Index() {
   useEffect(() => {
@@ -17,17 +27,19 @@ export default function Index() {
 
   async function registerForPushNotificationsAsync() {
     const { status } = await Notifications.requestPermissionsAsync();
+    
     if (status !== 'granted') {
       alert('You need to enable permissions in settings.');
       return;
     }
   }
 
+  
   async function schedulePushNotification(date:Date) {
-    await Notifications.scheduleNotificationAsync({
+    await Notifications.scheduleNotificationAsync({ 
       content: {
         title: "está na hora de sair",
-        body: 'vá para a Fateec Mauá',
+        body: 'vá para a Fatec Mauár',
         data: { data: 'geo:0,0?q="FATEC Maua"' },
       },
       trigger: date,
